14d268e38fe8381c38afc0ea6bbd9c2a7019ebbb
Son Huynh Van <sonhv@student.passerellesnumeriques.org>
2016-06-15 10:10:54 +0700

    design header layout and make controller pass object (UserModel)

--- App/ticketevent-web/src/main/java/pnv/intern/pyco/ticketevent/web/controller/DemoController.java
+++ App/ticketevent-web/src/main/java/pnv/intern/pyco/ticketevent/web/controller/DemoController.java
@@ -1,13 +1,24 @@
 package pnv.intern.pyco.ticketevent.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pnv.intern.pyco.ticketevent.services.DemoService;
+import pnv.intern.pyco.ticketevent.services.model.UserModel;
+
 @Controller
 public class DemoController {
+	
+	private DemoService demoService = new DemoService();
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String helloWorld() {
+	public String helloWorld(Model model) {
+		UserModel user = demoService.getUser();
+		
+		model.addAttribute("user", user);
+		
 		return "header_layout";
 	}
 }
